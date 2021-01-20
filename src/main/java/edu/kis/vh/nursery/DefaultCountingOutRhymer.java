1ef094cf40b977aa8166cebfc4f4a7e27313417e
kondziak <222534@edu.p.lodz.pl>
2021-01-20 12:36:46 +0100

    Zedytowano finale z powodu wcześniej występujących błędów

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -7,7 +7,7 @@ public class DefaultCountingOutRhymer {
     public static final int RETURN_VALUE = -1;
     public static final int MAX = 11;
     public int total = TOTALSTATUS;
-    private int[] numbers = new int[SIZE];
+    private final int[] numbers = new int[SIZE];
 
     public void countIn(int in) {
         if (!isFull())
